00bfade8186bc9b647db2c7d489efe226ee58f40
ompandey07 <iamompandey.it@gmail.com>
2024-09-08 16:24:45 +0545

    Dashboard Logs Employess Logins All Created

--- database/urls.py
+++ database/urls.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from database import views
 urlpatterns = [
-    path('home/', views.home),
+
+    # Lgins & Registations 
+    path('login_page/', views.login_page , name="login_page"),
+
+
+    # admin & admin controls 
+    path('admin_dashboard/', views.admin_dashboard , name="admin_dashboard"),
+    path('add_employe/', views.add_employe , name="add_employe"),
+    path('actyvity_log/', views.actyvity_log , name="actyvity_log"),
+
+
+    # Users & Users Controls 
+    path('users_dashboard/', views.users_dashboard , name="users_dashboard"),
+
+
 ]
--- database/views.py
+++ database/views.py
@@ -4,5 +4,19 @@ from django.http import HttpResponse
 
 
 
-def home(req):
-    return HttpResponse ('sdkfbsbdfjs d')
+def login_page (req):
+    return render (req, "index.html")
+
+
+def admin_dashboard(req):
+    return render (req , "Admin/admin_dashboard.html")
+
+def add_employe(req):
+    return render (req , 'Admin/add_employe.html')
+
+def actyvity_log(req):
+    return render (req , "Admin/activity_log.html")
+
+
+def users_dashboard(req):
+    return render (req , "Users/users_dashboard.html")
